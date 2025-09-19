98a808601c44d66d364fc5fc4c86f4adcbb385ff
Contrerasjosed1 <davidarenas05@gmail.com>
2025-09-18 23:45:58 -0500

    correccion distribución de gráficos

--- cnc_dash/pages/home.py
+++ cnc_dash/pages/home.py
@@ -8,15 +8,45 @@ dash.register_page(__name__, path="/", name="Home")
 layout = dbc.Container([
     # SECTION 1
     section("Clusters a partir de la densidad poblacional y la adopción digital", [
-        dbc.Row([
-            dbc.Col(html.Img(id="img-map", className="map-img"), md=6),
-            dbc.Col(dbc.Card(dbc.CardBody(dcc.Graph(id="fig-prom-pobl"))), md=3),
-            dbc.Col(dbc.Card(dbc.CardBody(dcc.Graph(id="fig-cobertura-cluster"))), md=3),
-        ], className="gy-3"),
-        dbc.Row([
-            dbc.Col(dbc.Card(dbc.CardBody(dcc.Graph(id="fig-adop-por-cluster"))), md=6)
-        ], className="gy-3 mt-1")
-    ]),
+    # Fila principal: Mapa (izq) y dos charts apiladas (der)
+    dbc.Row([
+        # Columna izquierda: Mapa
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody(
+                    html.Img(id="img-map", className="map-img",
+                             style={"display": "block", "width": "100%", "height": "100%", "objectFit": "contain"})
+                ),
+            ),
+            md=6, className="mb-3"
+        ),
+
+        # Columna derecha: dos gráficas apiladas
+        dbc.Col([
+            dbc.Row(
+                dbc.Col(
+                    dbc.Card(dbc.CardBody(dcc.Graph(id="fig-prom-pobl"))),
+                    width=12
+                ),
+                className="mb-3"
+            ),
+            dbc.Row(
+                dbc.Col(
+                    dbc.Card(dbc.CardBody(dcc.Graph(id="fig-cobertura-cluster"))),
+                    width=12
+                )
+            ),
+        ], md=6, className="mb-3"),
+    ], className="gy-3"),
+
+    # Fila inferior: gráfica a lo ancho (armoniza el flujo visual)
+    dbc.Row([
+        dbc.Col(
+            dbc.Card(dbc.CardBody(dcc.Graph(id="fig-adop-por-cluster"))),
+            md=12
+        )
+    ], className="gy-3 mt-1"),
+]),
 
     # SECTION 2
     section("Clusters a partir de las variables sociodemográficas y la adopción digital", [
